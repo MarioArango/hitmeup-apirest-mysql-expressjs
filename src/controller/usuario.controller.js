275efa2f7dc3af0fdb7c094e1581295fefc3be4e
ITD-VISION <jd.sarmientoh.7@gmail.com>
2019-10-22 14:46:02 -0500

    token

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -2,10 +2,11 @@ const usuario_controller = {};
 const mysql = require('../database/database');
 const bcrypt = require('bcrypt');
 var nodemailer = require('nodemailer');
+const jwt = require('../middlewares/jwt');
 
 usuario_controller.listar_comunidad_usuario = function(req, res){
  
-    var id_usuario = req.params.id_usuario;
+    const id_usuario = (jwt.decodificarToken(req.headers.authorization)).id_usuario;
     console.log(id_usuario);
     const sql = "call SP_GET_ListarComunidad(?)";
     mysql.query(sql, id_usuario,(err, comunidad)=>{
@@ -15,27 +16,28 @@ usuario_controller.listar_comunidad_usuario = function(req, res){
             res.status(400).send({ status: 'Error', message: 'Error.', error: err, code: 400});
         }
     });
-}
-  usuario_controller.login = function(req,res){
+};
+
+usuario_controller.login = function(req,res){
     const params = req.body;
     const nick = params.nick_usuario;
-     const password = params.password_usuario;
+    const password = params.password_usuario;
      console.log(nick,password);     
      const sql = "call SP_GET_ValidarUsuarioLogin(?)";
       mysql.query(sql,nick,(err, usuario)=>{
           if(!err){
               if(usuario[0][0]!= undefined){
-                //   bcrypt.compare(password, usuario[0][0].password, function(err_bcrypt,res_bcrypt){
-                      if(usuario[0][0].password === password){
-                          if(usuario[0][0].estado_usuario=="1"){
-                            res.status(200).send({status:'Success', usuario: usuario[0][0], code:200}); 
-                          }else{
-                            res.status(403).send({ status: 'Error', message: 'Usuario no habilitado', code:403 });
-                          }
+                bcrypt.compare(password, usuario[0][0].password, function(err_bcrypt,res_bcrypt){
+                    if (res_bcrypt) {
+                      if(usuario[0][0].estado_usuario=="1"){
+                        res.status(200).send({status:'Success', usuario: usuario[0][0], token:jwt.crearToken(usuario[0][0]),code:200}); 
                       }else{
-                        res.status(404).send({ status: 'Error', message: 'Contraseña Incorrecta', code:404 });
+                        res.status(403).send({ status: 'Error', message: 'Usuario no habilitado', code:403 });
                       }
-                //   });
+                    }else{
+                      res.status(404).send({ status: 'Error', message: 'Contraseña Incorrecta', code:404 });
+                    }
+                 });
               }else{
                   res.status(404).send({status:'Error', message: 'Usuario Incorrecto.', code:404});
               }
@@ -43,8 +45,9 @@ usuario_controller.listar_comunidad_usuario = function(req, res){
             res.status(400).send({ status: 'Error', error: err, code: 400});
         }
     });
- }
- usuario_controller.cambiar_conexion_usuario = function(req, res){
+};
+
+usuario_controller.cambiar_conexion_usuario = function(req, res){
    const params = req.body;
    const id_usuario =  params.id_usuario;
    const estado_usuario = params.estado_usuario;
@@ -57,8 +60,9 @@ usuario_controller.listar_comunidad_usuario = function(req, res){
         res.status(400).send({ status: 'Error', error: err, code: 400});
       }
     });
- }
- usuario_controller.recuperar_password = function(req, res){
+};
+
+usuario_controller.recuperar_password = function(req, res){
    const params = req.body;
    const email = params.email;
    const nick = params.nick;
@@ -94,7 +98,7 @@ usuario_controller.listar_comunidad_usuario = function(req, res){
     }
   });
  
-}
+};
 
 
 module.exports = usuario_controller;
--- src/routes/usuario.route.js
+++ src/routes/usuario.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../middlewares/authentication');
 const usuario_controller = require('../controller/usuario.controller');
 
-router.get('/listar-comunidad/:id_usuario',usuario_controller.listar_comunidad_usuario);
+router.get('/listar-comunidad/:id_usuario',auth.ensureAuth,usuario_controller.listar_comunidad_usuario);
 router.post('/login',usuario_controller.login);
 router.put('/cambiar-conexion-usuario',usuario_controller.cambiar_conexion_usuario);
 router.post('/recuperar-password',usuario_controller.recuperar_password);
